ee57d7d8697d5faeeaf982ecc95d4aaac161b8ff
J.-Fabian Wenisch <wenisch@madana.io>
2018-05-23 22:12:17 +0200

    DEV-230 #time 10m #comment Refactored RestClient so an instance can be created for every session instead of the static functions

--- src/main/java/com/spring/controller/LoginController.java
+++ src/main/java/com/spring/controller/LoginController.java
@@ -6,18 +6,24 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import de.madana.common.restclient.MDN_RestClient;
+
 @Controller
 public class LoginController 
 {
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String init(Model model) {
+	public String init(Model model) 
+	{
 		model.addAttribute("msg", "Please Enter Your Login Details");
 		return "login";
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String submit(Model model, @ModelAttribute("loginBean") LoginBean loginBean) {
-		if (loginBean != null && loginBean.getUserName() != null & loginBean.getPassword() != null) {
+	public String submit(Model model, @ModelAttribute("loginBean") LoginBean loginBean) 
+	{
+		MDN_RestClient oClient = new MDN_RestClient();
+		if (loginBean != null && loginBean.getUserName() != null & loginBean.getPassword() != null) 
+		{
 			if (loginBean.getUserName().equals("chandra") && loginBean.getPassword().equals("chandra123")) 
 			{
 				model.addAttribute("msg", loginBean.getUserName());
